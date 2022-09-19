c3be8314b6702ae7fb00ceb92fdcbdb8ca88376b
Cecil King <cecil.king@roadmunk.com>
2022-09-19 09:59:44 -0400

    allow running tests in parallel

--- wdio.conf.js
+++ wdio.conf.js
@@ -22,13 +22,8 @@ exports.config = {
     // then the current working directory is where your `package.json` resides, so `wdio`
     // will be called from there.
     //
-    // specs: [
-    //     './test/specs/**/*.js'
-    // ],
     specs: [
-      [
         './test/specs/**/*.js'
-      ]
     ],
     // Patterns to exclude.
     exclude: [
